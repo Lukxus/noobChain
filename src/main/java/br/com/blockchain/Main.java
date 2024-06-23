791400097f1d37130647e477d019bea60dd35509
Luiz Saraiva <luizsaraiva50@gmail.com>
2024-06-22 23:46:48 -0300

    Criando noobchain

---
+++ src/main/java/br/com/blockchain/Main.java
@@ -0,0 +1,11 @@
+package br.com.blockchain;
+
+import br.com.blockchain.structure.Block;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
